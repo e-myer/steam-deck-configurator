fc7cd6eb502f1f89a964c9856eb602fcb220aae6
Amir Samir <amirsamir92@gmail.com>
2023-02-28 00:34:59 +0600

    Added deckyloader script

--- script.sh
+++ script.sh
@@ -7,6 +7,13 @@ sudo pacman -Syu
 flatpak update -y
 
 #install apps
+#flatpaks
 flatpak install org.cubocore.CoreKeyboard -y # CoreKeyboard
 flatpak install flathub com.github.debauchee.barrier -y # Barrier
 flatpak install flathub com.heroicgameslauncher.hgl -y #Heroic Launcher
+flatpak install flathub net.davidotek.pupgui2 -y #Proton GE
+flatpak install flathub io.github.philipk.boilr -y #BoilR
+flatpak install flathub com.github.tchx84.Flatseal -y #Flatseal
+
+#scripts
+curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh # deckyloader
