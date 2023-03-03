05ee0b77c0fbd243f2b7457cd91a665b20956ca5
Amir Samir <amirsamir92@gmail.com>
2023-03-04 00:14:43 +0600

    Added confirmation before running commands

--- script.sh
+++ script.sh
@@ -2,6 +2,41 @@
 
 source ./functions.sh
 
+echo "#! /usr/bin/bash
+
+source ./functions.sh
+
+#update all apps
+sudo pacman -Syu
+
+#update flatpaks
+flatpak update -y
+
+#install apps
+#flatpaks
+flatpak install org.cubocore.CoreKeyboard -y # CoreKeyboard
+flatpak install flathub com.github.debauchee.barrier -y # Barrier
+flatpak install flathub com.heroicgameslauncher.hgl -y #Heroic Launcher
+flatpak install flathub net.davidotek.pupgui2 -y #Proton GE
+flatpak install flathub io.github.philipk.boilr -y #BoilR
+flatpak install flathub com.github.tchx84.Flatseal -y #Flatseal
+
+install_deckyloader
+install_cryoutilities
+install_emudeck
+install_refind
+#fix_barrier
+
+These are the commands that will be run, continue? (y/n)"
+
+read continue
+
+if [[ "$continue" == "n" || ! "$continue" == "y" ]];
+then
+    echo "exiting..."
+    exit 0
+fi
+
 #update all apps
 sudo pacman -Syu
 
