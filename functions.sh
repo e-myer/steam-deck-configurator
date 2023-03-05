22ba9aa09cc615a3a4adcda72131f5557fc25fb3
Amir Samir <amirsamir92@gmail.com>
2023-03-06 01:36:36 +0600

    changed paths to files i create to $HOME/.deck_setup

--- functions.sh
+++ functions.sh
@@ -5,22 +5,22 @@ install_deckyloader() {
     RELEASE=$(curl -s 'https://api.github.com/repos/SteamDeckHomebrew/decky-loader/releases' | jq -r 'first(.[] | select(.prerelease == "false"))')
     VERSION=$(jq -r '.tag_name' <<< "${RELEASE}" )
 
-    if [ -f "$HOME/deckyloader_installed_version" ]
+    if [ -f "$HOME/.deck_setup/deckyloader_installed_version" ]
     then
-        DECKYLOADER_INSTALLED_VERSION=$(cat "$HOME/deckyloader_installed_version")
+        DECKYLOADER_INSTALLED_VERSION=$(cat "$HOME/.deck_setup/deckyloader_installed_version")
         echo "DeckyLoader Latest Version is $VERSION"
         echo "DeckyLoader Installed Version is $VERSION"
             if [ "$VERSION" != "$DECKYLOADER_INSTALLED_VERSION" ];
             then
                 echo "Installing Latest Version"
                 curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_release.sh | sh
-                echo "$VERSION" > "$HOME/deckyloader_installed_version"
+                echo "$VERSION" > "$HOME/.deck_setup/deckyloader_installed_version"
             else
                echo "Latest Version of DeckyLoader is already installed"
             fi
     else
         curl -L https://github.com/SteamDeckHomebrew/decky-loader/raw/main/dist/install_release.sh | sh
-        echo "$VERSION" > "$HOME/deckyloader_installed_version"
+        echo "$VERSION" > "$HOME/.deck_setup/deckyloader_installed_version"
     fi
 }
 
@@ -44,19 +44,19 @@ install_emudeck() {
 
 install_refind() {
     #Install and set up rEFInd botloader
-    mkdir -p "$HOME/deck_setup/build"
-    cd "$HOME/deck_setup/build" || exit
+    mkdir -p "$HOME/.deck_setup/build"
+    cd "$HOME/.deck_setup/build" || exit
 
     #check for a folder called "SteamDeck_rEFInd"
-    if [ -d "$HOME/deck_setup/build/SteamDeck_rEFInd" ] #if the folder SteamDeck_rEFInd exists
+    if [ -d "$HOME/.deck_setup/build/SteamDeck_rEFInd" ] #if the folder SteamDeck_rEFInd exists
     then
-        git -C "$HOME/deck_setup/build/SteamDeck_rEFInd" pull #update refind git, the rEFInd readme reccomends doing git status  instead, but I think that that is wrong
+        git -C "$HOME/.deck_setup/build/SteamDeck_rEFInd" pull #update refind git, the rEFInd readme reccomends doing git status  instead, but I think that that is wrong
     else
-        git -C "$HOME/deck_setup/build/SteamDeck_rEFInd" clone https://github.com/jlobue10/SteamDeck_rEFInd/
+        git -C "$HOME/.deck_setup/build/SteamDeck_rEFInd" clone https://github.com/jlobue10/SteamDeck_rEFInd/
     fi
     
     chmod +x "$HOME/deck_setup/build/SteamDeck_rEFInd/install-GUI.sh"
-    "$HOME/deck_setup/build/SteamDeck_rEFInd/install-GUI.sh"
+    "$HOME/.deck_setup/build/SteamDeck_rEFInd/install-GUI.sh"
 }
 
 fix_barrier() {
