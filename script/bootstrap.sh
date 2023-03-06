fe31a7e5b2d0acbda39d352e556a1b97a0e00e71
Amir Samir <amirsamir92@gmail.com>
2023-03-06 13:18:55 +0600

    Made paths in bootstrap file absolute and made script executable in bootstrap script

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,3 +1,4 @@
-git -C ../SteamDeck_rEFInd git init
-git -C ../SteamDeck_rEFInd git submodule update
+git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" git init
+git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" git submodule update
+chmod +x "$HOME/.deck_setup/build/steam-deck-configurator/script.sh"
 mkdir -p "$HOME/.deck_setup/"
