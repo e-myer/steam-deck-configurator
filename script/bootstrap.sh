9f43a8e66819d1c3465da042675006cf76c656a2
Amir Samir <amirsamir92@gmail.com>
2023-03-06 13:20:30 +0600

    fixed error in bootstrap file

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,4 +1,4 @@
-git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" git init
-git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" git submodule update
+git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" init
+git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" submodule update
 chmod +x "$HOME/.deck_setup/build/steam-deck-configurator/script.sh"
 mkdir -p "$HOME/.deck_setup/"
