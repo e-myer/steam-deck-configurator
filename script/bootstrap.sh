db85fcd1564b807abed731aeb6ea0ae549d2bbba
Amir Samir <amirsamir92@gmail.com>
2023-03-06 14:00:49 +0600

    Set git to init if not already init

--- script/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" init
-git -C "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" submodule update
-chmod +x "$HOME/.deck_setup/build/steam-deck-configurator/script.sh"
-mkdir -p "$HOME/.deck_setup/"
