96f739aa351fc02e13c7077154cf503b72ef0cb9
Amir Samir <amirsamir92@gmail.com>
2023-03-07 02:08:41 +0600

    removed cd from install refind function

--- functions.sh
+++ functions.sh
@@ -45,7 +45,6 @@ install_emudeck() {
 install_refind() {
     #Install and set up rEFInd botloader
     chmod +x "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd/install-GUI.sh"
-    cd "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd" || exit
     "$HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd/install-GUI.sh $pwd/SteamDeck_rEFInd"
 }
 
