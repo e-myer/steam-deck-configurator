0938deffb9ec57699ac11d6b225ae31d43d0a5bd
e-myer <amirsamir92@gmail.com>
2023-03-11 17:43:12 +0600

    Added windows script to install drivers

---
+++ windows.ps1
@@ -0,0 +1,7 @@
+cp /usb/steam_deck_stuff/windows_drivers/ $HOME/Desktop/ #copy windows drivers folder to desktop
+$HOME/Desktop/windows_drivers/Aerith Windows Driver_2209130944/220913a-383120E-2209130944/Setup.exe #install AERITH Driver by  running the exe
+$HOME/Desktop/windows_drivers/RTLWlanE_WindowsDriver_2024.0.10.137_Drv_3.00.0039_Win11.L/Setup.exe #install RTLWLANE windows driver
+$HOME/Desktop/windows_drivers/BayHub_SD_STOR_ installV3.4.01.89_W10W11_logoed_20220228/setup.exe #install BayHub driver
+$HOME/Desktop/windows_drivers/RTBlueR_FilterDriver_1041.3005_1201.2021_new_L/ #either installdriver.cmd or script.ps1 or what im not sure
+pnputil /add-driver $HOME/Desktop/windows_drivers/NAU88L21_x64_1.0.6.0_WHQL - DUA_BIQ_WHQL/NAU88L21.inf /install #install NAU88L21 driver
+pnputil /add-driver $HOME/Desktop/windows_drivers/cs35l41-V1.2.1.0/cs35l41.inf /install # install cs35l41 driver
