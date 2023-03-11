ea9dfe7dfc788e4ab67a9505f53e635933b549c9
e-myer <amirsamir92@gmail.com>
2023-03-11 17:00:01 +0600

    deleted bootstrap.sh

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d $HOME/.deck_setup/build/steam-deck-configurator/SteamDeck_rEFInd/.git ]
-then
-git submodule init
-git submodule update
-fi
-chmod +x "$HOME/.deck_setup/build/steam-deck-configurator/script.sh"
