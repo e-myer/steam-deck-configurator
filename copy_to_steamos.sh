06ddab6c2ec06a36186030be815f0e5cf2f116f1
e-myer <amirsamir92@gmail.com>
2023-03-12 23:19:47 +0600

    make files executable

--- copy_to_steamos.sh
+++ copy_to_steamos.sh
@@ -6,4 +6,4 @@ mkdir $HOME/.deck_setup
 mkdir $HOME/.deck_setup/applications
 cp -vr functions.sh configure_deck.sh SteamDeck_rEFInd $HOME/.deck_setup/
 cp -v ../Steam-ROM-Manager-2.3.40.AppImage $HOME/.deck_setup/applications
-chmod +x 
+chmod +x $HOME/.deck_setup/functions.sh $HOME/.deck_setup/configure_deck.sh $HOME/.deck_setup/SteamDeck_rEFInd/SteamDeck_rEFInd_install.sh $HOME/.deck_setup/SteamDeck_rEFInd/install-GUI.sh
