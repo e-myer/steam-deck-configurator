f7c37e042b7ee32ab66bea774af677baad847a63
e-myer <amirsamir92@gmail.com>
2023-03-13 18:13:20 +0600

    Added a dot to run the powershell script

--- windows.ps1
+++ windows.ps1
@@ -4,4 +4,4 @@ $HOME/Desktop/windows_drivers/BayHub_SD_STOR_ installV3.4.01.89_W10W11_logoed_20
 $HOME/Desktop/windows_drivers/RTBlueR_FilterDriver_1041.3005_1201.2021_new_L/InstallDriver.cmd # install RTBlueR_FilterDriver
 pnputil /add-driver $HOME/Desktop/windows_drivers/NAU88L21_x64_1.0.6.0_WHQL - DUA_BIQ_WHQL/NAU88L21.inf /install #install NAU88L21 driver
 pnputil /add-driver $HOME/Desktop/windows_drivers/cs35l41-V1.2.1.0/cs35l41.inf /install # install cs35l41 driver
-$HOME/Windows/.deck_setup/bootsequence-rEFInd-first.ps1
+. $HOME/Windows/.deck_setup/bootsequence-rEFInd-first.ps1
