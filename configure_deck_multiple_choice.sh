190b6b93235749fd85dfdb98835ce70da97b6170
e-myer <amirsamir92@gmail.com>
2023-03-14 19:14:04 +0600

    removed read continue text

--- configure_deck_multiple_choice.sh
+++ configure_deck_multiple_choice.sh
@@ -6,15 +6,6 @@ echo ""
 read -p "Please make sure a sudo password is already set before continuing. If you have not set the user\
  or sudo password, please exit this installer with 'Ctrl+c' and then create a password either using 'passwd'\
  from a command line or by using the KDE Plasma User settings GUI. Otherwise, press Enter/Return to continue with the install."
- 
-
-read continue
-
-if [[ "$continue" == "n" || ! "$continue" == "y" ]];
-then
-    echo "exiting..."
-    exit 0
-fi
 
 echo -e "0. Default (starred) \n \
 * 1. Update apps \n \
