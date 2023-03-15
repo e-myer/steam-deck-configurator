a2f79935ac21a5c7bec48de19237336540d94eeb
e-myer <amirsamir92@gmail.com>
2023-03-16 00:42:29 +0600

    removed quotes in script

--- configure_deck.sh
+++ configure_deck.sh
@@ -54,14 +54,14 @@ tasks=( "echo default" \
 default_tasks=( 1 2 3 4 5 6 7 8 9 10 11 12 13 ) # edit these numbers to edit the default tasks
 
 read string
-echo $string
+echo "$string"
 if [ "$string" == 0 ];
 then
     echo "string is 0"
     for i in "${default_tasks[@]}";
     do
         echo "${tasks[$i]}"
-#        ${tasks[$i]} #run each task in default tasks array
+        ${tasks[$i]} #run each task in default tasks array
     done
 else
     echo "string is not 0" # if default tasks isn't chosen
@@ -69,6 +69,6 @@ else
     for i in "${chosen_tasks[@]}"
     do
         echo "${tasks[$i]}" #echo the task for each
-#        "${tasks[$i]}" # run the tasks 
+        ${tasks[$i]} # run the tasks 
     done
 fi
