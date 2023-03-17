e1448f91acd4645473b7e8c54a6e225923450f85
e-myer <amirsamir92@gmail.com>
2023-03-17 12:30:54 +0600

    changed pipe to two arrows

--- configure_deck_gui.sh
+++ configure_deck_gui.sh
@@ -44,7 +44,7 @@ amount_of_chosen_tasks=${#chosen_tasks[@]}
 for i in $indexes_of_chosen_tasks
 do
     (( task_number++ ))
-    ${tasks[$i]} | tee terminal_output & # run the tasks 
+    ${tasks[$i]} >> terminal_output & # run the tasks 
     qdbus $dbusRef Set "" value $i & 
     qdbus $dbusRef setLabelText "$task_number/$amount_of_chosen_tasks: ${tasks[$i]}: $(tail --lines 1 ./terminal_output)" &
     wait
