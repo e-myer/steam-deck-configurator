b59b59acf00fa2aa6b711773de3b135c6d1f6daa
e-myer <amirsamir92@gmail.com>
2023-03-19 18:51:11 +0600

    edited default tasks array to reflact list changes

--- configure_deck_cli.sh
+++ configure_deck_cli.sh
@@ -33,7 +33,7 @@ D. Default (starred) \n \
   21. Fix_barrier \n \n \
 Which tasks to run? (0 for all the default tasks)"
 
-default_tasks=( 0 1 2 3 4 5 6 7 8 10 11 12 13 14 15 ) # edit these numbers to edit the default tasks
+default_tasks=( 0 1 2 3 4 5 6 7 8 9 11 12 13 14 ) # edit these numbers to edit the default tasks
 
 read string
 echo $string
