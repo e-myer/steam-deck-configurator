185e297e22cca3ce05fe3ca382dd74aa7d46ef9e
e-myer <amirsamir92@gmail.com>
2023-03-20 21:25:52 +0600

    removed gui from filenames

--- copy_to_steamos.sh
+++ copy_to_steamos.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
 mkdir -p $HOME/.deck_setup/applications $HOME/.deck_setup/steam-deck-configurator
-cp -vr functions.sh configure_deck_gui.sh SteamDeck_rEFInd rEFInd_configs $HOME/.deck_setup/steam-deck-configurator/
+cp -vr functions.sh configure_deck.sh SteamDeck_rEFInd rEFInd_configs $HOME/.deck_setup/steam-deck-configurator/
 cp -v ../applications/Steam-ROM-Manager-2.3.40.AppImage $HOME/.deck_setup/applications/
-chmod +x $HOME/.deck_setup/steam-deck-configurator/functions.sh $HOME/.deck_setup/steam-deck-configurator/configure_deck_gui.sh $HOME/.deck_setup/steam-deck-configurator/SteamDeck_rEFInd/SteamDeck_rEFInd_install.sh $HOME/.deck_setup/steam-deck-configurator/SteamDeck_rEFInd/install-GUI.sh
+chmod +x $HOME/.deck_setup/steam-deck-configurator/functions.sh $HOME/.deck_setup/steam-deck-configurator/configure_deck.sh $HOME/.deck_setup/steam-deck-configurator/SteamDeck_rEFInd/SteamDeck_rEFInd_install.sh $HOME/.deck_setup/steam-deck-configurator/SteamDeck_rEFInd/install-GUI.sh
