35ff881b94c6fa0735a4fb1655d5b77336eaba08
emyer <39300489+e-myer@users.noreply.github.com>
2023-03-21 21:20:28 +0600

    deleted windows script

--- copy_to_windows.ps1
+++
@@ -1,3 +0,0 @@
-mkdir $HOME/Desktop/.deck_setup
-cp /usb/steam_deck_stuff/windows_drivers/ $HOME/.deck_setup #copy windows drivers folder to desktop
-cp SteamDeck_rEFInd/Windows/bios_install_prep.ps1 $HOME/Windows/.deck_setup
