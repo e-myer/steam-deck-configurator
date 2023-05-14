9c5a9e89a01b504e56edc8ede60168e41322e70b
emyer <39300489+e-myer@users.noreply.github.com>
2023-05-14 20:36:24 +0600

    made functions.sh path absolute

--- configure_deck.sh
+++ configure_deck.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-source ./functions.sh
+source $HOME/.deck_setup/steam-deck-configurator/functions.sh
 
 kdialog --title "password" --yesno "Please make sure a sudo password is set before continuing. If you have not set the sudo password, set it first. Continue?"
 
