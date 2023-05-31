b55a0fa95553917efd52cd76f4ae0ec2c13e2054
emyer <39300489+e-myer@users.noreply.github.com>
2023-05-31 18:55:11 +0600

    only add to flatpak_list if it isn't there

--- functions.sh
+++ functions.sh
@@ -120,12 +120,9 @@ export_flatpaks() {
     do
         print_log "exporting ${flatpak_names[$flatpak]}"
         flatpak --verbose create-usb "$configurator_dir/flatpaks" "${flatpak_ids[$flatpak]}"
-        if [ -z "$flatpak_index" ]; then
-            local flatpak_index=0
-        else
-            ((flatpak_index ++))
+        if grep -qxF "${flatpak_names[$flatpak]}=${flatpak_ids[$flatpak]}" "$configurator_dir/flatpaks_list"; then
+            echo "${flatpak_names[$flatpak]}=${flatpak_ids[$flatpak]}" >> "$configurator_dir/flatpaks_list"
         fi
-        echo "${flatpak_names[$flatpak]}"="${flatpak_ids[$flatpak]}" >> "$configurator_dir/flatpaks_list"
     done
 }
 
