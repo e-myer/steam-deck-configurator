c0001582d06c79d2f0c06b4d37566d3cf5f11835
emyer <39300489+e-myer@users.noreply.github.com>
2023-06-25 01:03:56 +0600

    commented echoes

--- zenity_rewrite.sh
+++ zenity_rewrite.sh
@@ -24,15 +24,15 @@ FALSE "echoes_he" "echo he"'
 
 create_dialog() {
     while true; do
-        readarray -t chosen_tasks < <(echo "$menu" | xargs zenity --list --checklist --separator=$'\n' --column=status --column=task --column=label)
-        echo "${chosen_tasks[@]}"
+        readarray -t chosen_tasks < <(echo "$menu" | xargs zenity --list --checklist --separator=$'\n' --column=status --column=task --column=label --print-column=2 --hide-column=2)
+        #echo "${chosen_tasks[@]}"
         run_tasks
     done
 }
 
 run_tasks() {
-    echo "${chosen_tasks[@]}"
-    echo "${#chosen_tasks[@]}"
+    #echo "${chosen_tasks[@]}"
+    #echo "${#chosen_tasks[@]}"
 
     for chosen_task in "${chosen_tasks[@]}"; do
         $chosen_task
