318c128c24e9e8937c76d668993d7807f7f864fb
emyer <39300489+e-myer@users.noreply.github.com>
2023-06-24 14:49:01 +0600

    commit

--- zenity_rewrite.sh
+++ zenity_rewrite.sh
@@ -1,2 +1,23 @@
 #! /usr/bin/bash
 
+# Configures various functions in a Steam Deck
+
+configurator_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echoes_hi() {
+    echo hi
+}
+
+echoes_ho() {
+    echo ho
+}
+
+echoes_he() {
+    echo he
+}
+
+menu='FALSE "echoes_hi" "echo hi" \
+FALSE "echoes_ho" "echo ho" \
+FALSE "echoes_he" "echo he"'
+
+echo "$menu" | xargs zenity --list --checklist --separator=$'\n' --column=status --column=task --column=label
